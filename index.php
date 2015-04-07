e8567c80fce2531b08d9a6af653db7324ad736b1
hodoug <j.hodoug@gmail.com>
2015-04-07 20:05:07 +0900

    Animated Background Header를 삽입.

--- index.php
+++ index.php
@@ -1,3 +1,15 @@
-<?php
-echo 'www2015';
-?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8" />
+    </head>
+    <body>
+        <div id="large-header" class="large-header">
+            <canvas id="demo-canvas"></canvas>
+            <script src="contribute/animatedHeader/TweenLite.min.js"></script>
+            <script src="contribute/animatedHeader/EasePack.min.js"></script>
+            <script src="contribute/animatedHeader/rAF.js"></script>
+            <script src="contribute/animatedHeader/demo-1.js"></script>
+        </div>
+    </body>
+</html>
