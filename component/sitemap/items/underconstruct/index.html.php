1b02afa7573e28b72e95a3561048fde1263b3b52
Kyuman Hwang <hwangkm@member.jinbo.net>
2015-08-12 01:40:46 +0900

    이용약관, 사이트맵 개발중 페이지 추가

---
+++ component/sitemap/items/underconstruct/index.html.php
@@ -0,0 +1,7 @@
+<article class="underconstruct component  <?php print $classes; ?>" style="<?php print $style; ?>">
+	<div class="underconstruct-container">
+		<i class="j-movement-hammer-and-sickle"></i>
+		<h3>페이지 공사중</h3>
+		<p class="notice">조만간 열립니다, 기다려 주세요.</p>
+	</div>
+</article>
---
+++ component/terms/items/terms/index.html.php
@@ -0,0 +1,5 @@
+<article class="terms component  <?php print $classes; ?>" style="<?php print $style; ?>">
+	<div class="terms-container">
+		<?php print $data['description']; ?>
+	</div>
+</article>
