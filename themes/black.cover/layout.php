3ae05b8ee715ce88d03a1bd0d79e032edc7630a7
taiji202 <taiji202@gmail.com>
2015-08-18 02:23:26 +0900

    OG Meta 테그 추가

--- themes/black.cover/layout.php
+++ themes/black.cover/layout.php
@@ -10,16 +10,23 @@ if(!is_object($browser)) $browser = new Browser();
 	<link rel="shortcut icon" href="<?php print url("resources/images/favicon.ico"); ?>">
 	<title>소통과 연대의 즐거움 : 진보넷</title>
 
+<?php	echo $this->header(); ?>
+
+	<meta property="fb:app_id" content="497213570307119"/>
 	<meta property="og:title" content="진보네트워크센터"/>
 	<meta property="og:type" content="website"/>
 	<meta property="og:url" content="http://www.jinbo.net"/>
-	<meta property="og:image" content="<?php print url("resources/images/og.png"); ?>"/>
+	<meta property="og:image" content="https://www.jinbo.net<?php print url("resources/images/og.png"); ?>"/>
 	<meta property="og:description" content="검열과 감시에 맞서 정보인권을 지키고 자본과 국가로부터 독립적인 네트워크를 구축하는 정보인권단체 진보넷의 활동을 안내합니다.">
 	<meta property="og:site_name" content="진보네트워크센터"/>
 	<meta property="og:updated_time" content=""/>
 
-<?php	echo $this->header(); ?>
-
+	<meta name="twitter:card" content="summary"/>
+	<meta name="twitter:title" content="진보네트워크센터"/>
+	<meta name="twitter:description" content="검열과 감시에 맞서 정보인권을 지키고 자본과 국가로부터 독립적인 네트워크를 구축하는 정보인권단체 진보넷의 활동을 안내합니다."/>
+	<meta name="twitter:creator" content="jinbonet"/>
+	<meta name="twitter:image:src" content="https://www.jinbo.net<?php print url("resources/images/og.png"); ?>"/>
+	<meta name="twitter:url" content="http://www.jinbo.net"/>
 </head>
 <body class="jinbo-net <?php print $breadcrumbs_class; ?>">
 	<header id="site-header">
