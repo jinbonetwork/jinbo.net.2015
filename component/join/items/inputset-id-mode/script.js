e8ea18c83be3977de1e99cfde74a20f8b2ffb601
hodoug <j.hodoug@gmail.com>
2015-12-11 15:38:11 +0900

    회원가입페이지] 각 아이템의 스크립트 중 독립적이지 않은 것은 app-join.js로 옮겼다.

--- component/join/items/inputset-id-mode/script.js
+++ component/join/items/inputset-id-mode/script.js
@@ -14,34 +14,6 @@
 		}).change(function(){
 			$iim('.radio-button').removeClass('checked');
 			$(this).siblings('.radio-button').addClass('checked');
-			if($(this).val() === 'old'){
-				$('.hide-when-user').hide();
-				$('.show-when-user').show();
-				$.ajax({
-					url: $(location).attr('href'), type: 'post',
-					async: false,
-					data: {todo: 'is-logged-in'},
-					success: function(result){
-						if(result){
-							result = $.parseJSON(result);
-							$('.hide-when-loggedin').hide();
-							$('.show-when-loggedin').show();
-							$('input[type="text"][name="userId"]').val(result.id);
-							if(result.isMember){
-								$('.hide-when-loggedin-member').hide();
-								$('.show-when-loggedin-member').show();
-							}
-						}
-					}
-				});
-			} else {
-				$('.hide-when-user').show();
-				$('.show-when-user').hide();
-				$('.hide-when-loggedin-member').show();
-				$('.show-when-loggedin-member').hide();
-				$('.hide-when-loggedin').show();
-				$('.show-when-loggedin').hide();
-			}
 		});
 	});
 })(jQuery);
--- resources/script/app-join.js
+++ resources/script/app-join.js
@@ -3,5 +3,100 @@
 		$('input[type="text"]').keydown(function(event){
 			if(event.which == 13) event.preventDefault();
 		});
-	});
+		//inputset-id-mode
+		$iim('input[type="radio"]').change(function(){
+			if($(this).val() === 'old'){
+				$('.hide-when-user').hide();
+				$('.show-when-user').show();
+				$.ajax({
+					url: $(location).attr('href'), type: 'post',
+					async: false,
+					data: {todo: 'is-logged-in'},
+					success: function(result){
+						if(result){
+							result = $.parseJSON(result);
+							$('.hide-when-loggedin').hide();
+							$('.show-when-loggedin').show();
+							$('input[type="text"][name="userId"]').val(result.id);
+							if(result.isMember){
+								$('.hide-when-loggedin-member').hide();
+								$('.show-when-loggedin-member').show();
+							}
+						}
+					}
+				});
+			} else {
+				$('.hide-when-user').show();
+				$('.show-when-user').hide();
+				$('.hide-when-loggedin-member').show();
+				$('.show-when-loggedin-member').hide();
+				$('.hide-when-loggedin').show();
+				$('.show-when-loggedin').hide();
+			}
+		});
+		//inputset-submit
+		$is('button[type="submit"]').click(function(event){
+			event.preventDefault();
+
+			var data = $is().closest('form').serializeArray();
+			var dataObj = {};
+			for(var i = 0, len = data.length; i < len; i++){
+				dataObj[data[i].name] = data[i].value;
+			}
+			dataObj.todo = 'submit';
+
+			$.ajax({
+				url: $(location).attr('href'), type: 'post', async: false,
+				data: dataObj,
+				success: function(result){
+					if(result){
+						result = $.parseJSON(result);
+						var name = result[0];
+						var message = result[1];
+						var $input = $('input[name="'+name+'"]');
+						if(name === 'etc_youMember'){
+							$('input[name="userId"]').focus();
+						}
+						else if(name === 'etc_login_youMember'){
+							$('button[name="login"]').click();
+							$('input[name="userId"]').focus();
+						}
+						else if(name === 'agreePrivate' || name === 'agreeService' || name === 'agreeCms'){
+							$input.focus();
+						}
+						else if(name === 'etc_error'){
+							alert(message);
+						} else {
+							$input.focus();
+							$input.closest('article').find('span.error-message').text(message);
+							$input.keyup(function(){
+								$(this).closest('article').find('span.error-message').text('');
+							});
+						}
+					}
+					else {
+						$('#join-section2').hide();
+
+						var date = new Date(); date = date.getFullYear() + '-' + (date.getMonth()+1) + '-' + date.getDate();
+						var params = '?name='+dataObj.name+'&request_fee='+dataObj.donation+'&phone='+dataObj.phone+
+							'&bank='+dataObj.bank+'&acct_id='+dataObj.acctnum+'&acct_name='+dataObj.acctowner+
+							'&acct_personal_id='+dataObj.registnum+'&request_date='+date;
+						var lclEvent = $.Event('load-cms-license');
+						lclEvent.params = params;
+
+						$('.inputset-complete').trigger(lclEvent);
+					}//end fo if(result)
+				}//end of success:
+			});//end of $.ajax()
+		});//end of click()
+	});//end of ready()
+	function $iim(selector, $obj){
+		if($obj) return $obj.closest('.inputset-id-mode').find(selector);
+		else return $('.inputset-id-mode').find(selector);
+	}
+	function $is(selector, $obj){
+		if(selector && $obj) return $obj.closest('.inputset-submit').find(selector);
+		else if(selector) return $('.inputset-submit').find(selector);
+		else return $('.inputset-submit');
+	}
 })(jQuery);
