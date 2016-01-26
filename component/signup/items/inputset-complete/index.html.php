6cebca82ccb28e86af31efaf7078a31ef4105a3b
hodoug <j.hodoug@gmail.com>
2016-01-26 10:52:18 +0900

    이용자 가입페이지] 가입 완료 후 메시지 수정.

--- component/signup/items/inputset-complete/index.html.php
+++ component/signup/items/inputset-complete/index.html.php
@@ -1,8 +1,9 @@
 <article class="inputset-complete <?php print $classes; ?>" style="<?php print $style; ?>">
 	<div class="content">
 		<h1>이용자 가입이 완료되었습니다.</h1>
-		<p>이미 로그인된 상태이기 때문에 정상적으로 서비스를 이용하실 수 있습니다. 아래의 버튼 중 하나를 클릭하세요.</p>
-		<p>하지만 이메일을 사용하시기 위해서는 먼저 전화를 주시거나 이메일을 보내주셔야 합니다. 전화번호는 02-774-4551, 이메일 주소는 <a href="mailto:truesig@jinbo.net">truesig@jinbo.net</a>입니다.</p>
+		<p>하지만 24시간이 지난 후에야 정상적으로 서비스를 이용하실 수 있고, 이메일을 이용하시기 위해서는 전화를 주시거나 이메일을 보내주셔야 합니다.</p>
+		<p>급하게 진보넷의 서비스를 이용하셔야 하는 경우에도 전화를 주시거나 이메일을 보내주세요. </p>
+		<p>진보넷의 전화번호는 02-774-4551, 이메일 주소는 <a href="mailto:truesig@jinbo.net">truesig@jinbo.net</a>입니다.</p>
 		<a class="a-button" href="http://mail.jinbo.net">이메일</a>
 		<a class="a-button" href="http://blog.jinbo.net">블로그</a>
 		<a class="a-button" href="http://go.jinbo.net/cool">속보게시판</a>
