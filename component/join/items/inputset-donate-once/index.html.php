27afcb57a2047883e9fe27db044e142e5786a792
hodoug <j.hodoug@gmail.com>
2016-03-07 10:02:03 +0900

    일시후원 소셜펀치 주소 수정.

--- component/join/items/inputset-donate-once/index.html.php
+++ component/join/items/inputset-donate-once/index.html.php
@@ -2,7 +2,7 @@
 	<div class="top-background"></div>
 	<div class="article-wrap">
 		<div class="content">
-			<iframe src="https://aniso-funch.jinbo.net/anyorg/donate/jinbonet" width="100%" frameborder="no" scrolling="no"></iframe>
+			<iframe src="https://socialfunch.org/jinbonetwork/donate/jinbonet" width="100%" frameborder="no" scrolling="no"></iframe>
 		</div>
 	</div>
 </article>
